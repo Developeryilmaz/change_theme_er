4822ed8b71cd5aa71105b6eb69e0377174c46e5b
MacbookPro <developer.yilmazer@gmail.com>
2025-02-22 03:12:20 +0300

    add dart formatter

--- example/lib/screens/home_screen.dart
+++ example/lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:change_theme_er/change_theme_er.dart';
@@ -23,9 +22,10 @@ class HomeScreen extends StatelessWidget {
                 curve: Curves.easeInOut,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
-                    colors: isDark
-                        ? [Colors.black, Colors.grey.shade900]
-                        : [Colors.blue.shade100, Colors.white],
+                    colors:
+                        isDark
+                            ? [Colors.black, Colors.grey.shade900]
+                            : [Colors.blue.shade100, Colors.white],
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
                   ),
@@ -39,22 +39,29 @@ class HomeScreen extends StatelessWidget {
                   width: MediaQuery.of(context).size.width * 0.85,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(25),
-                    color: isDark
-                        ? Colors.white.withAlpha((0.1 * 255).toInt())
-                        : Colors.white.withAlpha((0.3 * 255).toInt()),
+                    color:
+                        isDark
+                            ? Colors.white.withAlpha((0.1 * 255).toInt())
+                            : Colors.white.withAlpha((0.3 * 255).toInt()),
                     boxShadow: [
                       BoxShadow(
-                        color: isDark
-                            ? Colors.black.withAlpha((0.5 * 255).toInt())
-                            : Colors.blue.shade200.withAlpha((0.5 * 255).toInt()),
+                        color:
+                            isDark
+                                ? Colors.black.withAlpha((0.5 * 255).toInt())
+                                : Colors.blue.shade200.withAlpha(
+                                  (0.5 * 255).toInt(),
+                                ),
                         blurRadius: 25,
                         spreadRadius: 5,
                       ),
                     ],
                     border: Border.all(
-                      color: isDark
-                          ? Colors.white.withAlpha((0.2 * 255).toInt())
-                          : Colors.blue.shade100.withAlpha((0.5 * 255).toInt()),
+                      color:
+                          isDark
+                              ? Colors.white.withAlpha((0.2 * 255).toInt())
+                              : Colors.blue.shade100.withAlpha(
+                                (0.5 * 255).toInt(),
+                              ),
                       width: 2,
                     ),
                   ),
@@ -64,8 +71,14 @@ class HomeScreen extends StatelessWidget {
                       // Theme Switch Icon
                       AnimatedSwitcher(
                         duration: const Duration(milliseconds: 500),
-                        transitionBuilder: (Widget child, Animation<double> animation) {
-                          return ScaleTransition(scale: animation, child: child);
+                        transitionBuilder: (
+                          Widget child,
+                          Animation<double> animation,
+                        ) {
+                          return ScaleTransition(
+                            scale: animation,
+                            child: child,
+                          );
                         },
                         child: Icon(
                           isDark ? Icons.nightlight_round : Icons.wb_sunny,
@@ -90,15 +103,24 @@ class HomeScreen extends StatelessWidget {
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(40),
                             gradient: LinearGradient(
-                              colors: isDark
-                                  ? [Colors.deepPurpleAccent, Colors.blueAccent]
-                                  : [Colors.orangeAccent, Colors.redAccent],
+                              colors:
+                                  isDark
+                                      ? [
+                                        Colors.deepPurpleAccent,
+                                        Colors.blueAccent,
+                                      ]
+                                      : [Colors.orangeAccent, Colors.redAccent],
                             ),
                             boxShadow: [
                               BoxShadow(
-                                color: isDark
-                                    ? Colors.deepPurpleAccent.withAlpha((0.5 * 255).toInt())
-                                    : Colors.orangeAccent.withAlpha((0.5 * 255).toInt()),
+                                color:
+                                    isDark
+                                        ? Colors.deepPurpleAccent.withAlpha(
+                                          (0.5 * 255).toInt(),
+                                        )
+                                        : Colors.orangeAccent.withAlpha(
+                                          (0.5 * 255).toInt(),
+                                        ),
                                 blurRadius: 20,
                                 spreadRadius: 5,
                               ),
@@ -112,9 +134,13 @@ class HomeScreen extends StatelessWidget {
                                 duration: const Duration(milliseconds: 500),
                                 curve: Curves.easeInOut,
                                 alignment:
-                                    isDark ? Alignment.centerRight : Alignment.centerLeft,
+                                    isDark
+                                        ? Alignment.centerRight
+                                        : Alignment.centerLeft,
                                 child: Container(
-                                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                                  margin: const EdgeInsets.symmetric(
+                                    horizontal: 5,
+                                  ),
                                   width: 50,
                                   height: 50,
                                   decoration: BoxDecoration(
@@ -122,19 +148,29 @@ class HomeScreen extends StatelessWidget {
                                     color: Colors.white,
                                     boxShadow: [
                                       BoxShadow(
-                                        color: isDark
-                                            ? Colors.deepPurpleAccent.withAlpha((0.5 * 255).toInt())
-                                            : Colors.orangeAccent.withAlpha((0.5 * 255).toInt()),
+                                        color:
+                                            isDark
+                                                ? Colors.deepPurpleAccent
+                                                    .withAlpha(
+                                                      (0.5 * 255).toInt(),
+                                                    )
+                                                : Colors.orangeAccent.withAlpha(
+                                                  (0.5 * 255).toInt(),
+                                                ),
                                         blurRadius: 15,
                                         spreadRadius: 3,
                                       ),
                                     ],
                                   ),
                                   child: Icon(
-                                    isDark ? Icons.nightlight_round : Icons.wb_sunny,
+                                    isDark
+                                        ? Icons.nightlight_round
+                                        : Icons.wb_sunny,
                                     size: 28,
                                     color:
-                                        isDark ? Colors.deepPurpleAccent : Colors.orangeAccent,
+                                        isDark
+                                            ? Colors.deepPurpleAccent
+                                            : Colors.orangeAccent,
                                   ),
                                 ),
                               ),
